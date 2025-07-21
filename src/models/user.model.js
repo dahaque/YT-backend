e4c776bc9f83fb34ed78e18d83fc9e382deaee16
Sadiq Nazirul <d3vil1189@gmail.com>
2025-07-21 13:05:00 +0530

    added token methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,8 +55,37 @@ userSchema.pre("save", async function(next){
   next();
 })
 
-userSchema.method.isPasswordCorrect = async function(password){
+// injecting methods in userSchema 
+userSchema.methods.isPasswordCorrect = async function(password){
  return await bcrypt.compare(password, this.password); // returns a boolean
+}
+
+userSchema.methods.generateAccessToken = function(){
+ return jwt.sign(
+    {
+      _id : this._id,
+      username : this.username,
+      email : this.email,
+      fullname : this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+ return jwt.sign(
+    {
+      _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
 }
 
 export const User = mongoose.model("User", userSchema)
