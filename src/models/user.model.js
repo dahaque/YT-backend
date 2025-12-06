74d84f42bccdfdf437eef0f0170109d142af5a30
Sadiq <161265120+dahaque@users.noreply.github.com>
2025-12-06 11:22:42 +0530

    refactor user model for consistent formatting and readability

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,52 +4,52 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
-    username : {
-      type : String,
-      required : true,
-      unique : true,
-      lowercase : true,
-      trim : true,
-      index : true //makes it more searchable
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true //makes it more searchable
     },
-    email : {
-      type : String,
-      required : true,
-      unique : true,
-      lowercase : true,
-      trim : true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullname : {
-      type : String,
-      required : true,
-      trim : true,
-      index : true
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true
     },
-    avatar : {
-      type : String, // cloudinary
-      required : true
+    avatar: {
+      type: String, // cloudinary
+      required: true
     },
-    coverImage : {
-      type : String, // cloudinary
+    coverImage: {
+      type: String, // cloudinary
     },
-    watchHistory : [
+    watchHistory: [
       {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Video"
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
       }
     ],
-    password : {
-      type : String,
-      required : [true, "password is required"]
+    password: {
+      type: String,
+      required: [true, "password is required"]
     },
-    refreshToken : {
-      type : String
+    refreshToken: {
+      type: String
     },
-    token :{
-      type : String,
-      default : ''
+    token: {
+      type: String,
+      default: ''
     }
-  }, {timestamps : true})
+  }, { timestamps: true })
 
  // encryption takes times that's why async function 
 userSchema.pre("save", async function(next){
