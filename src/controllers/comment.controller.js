a3fa7320fdfd8edfc2723b24a5a4a2734e17b9a1
Sadiq <161265120+dahaque@users.noreply.github.com>
2025-12-11 12:00:29 +0530

    add update, delete and get all comment functionalities with error handling

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,12 +5,51 @@ import { ApiResponse } from '../utils/ApiResponse.js';
 import { Comment } from '../models/comment.model.js'
 import { Video } from '../models/video.model.js'
 
-const getVideoComments = asyncHandler( async(req, res)  => {
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+const getVideoComments = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    const { page = 1, limit = 10 } = req.query
+
+    const pageNum = Number(page);
+    const limitNum = Number(limit);
+
+    if (!videoId) {
+        throw new ApiError(400, "Video ID not found!")
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found!")
+    }
+
+    if (pageNum <= 0)
+        throw new ApiError(400, "enter a valid pagenumber!")
+
+    if (limitNum <= 0)
+        throw new ApiError(400, "enter a valid limit!")
+
+    const skip = (pageNum - 1) * limitNum;
+
+    const comments = await Comment.find({ videoId })
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limitNum)
+        .populate("owner", "username avatar")
+
+    const totalComment = await Comment.countDocuments({ videoId })
+    const totalPages = Math.ceil(totalComment / limitNum);
+
+    return res.status(200).json({
+        page: pageNum,
+        limit: limitNum,
+        totalPages,
+        totalComment,
+        comments
+    });
+
 })
 
-const addComment = asyncHandler(async (req, res) => {
+const addComment = asyncHandler(async(req, res) => {
     const { videoId } = req.params;
     const { comment } = req.body;
 
@@ -43,7 +82,78 @@ const addComment = asyncHandler(async (req, res) => {
         )
 })
 
+const UpdateComment = asyncHandler(async(req, res) => {
+    const { commentId } = req.params;
+    const { updatedContent } = req.body;
+
+    if(!commentId){
+        throw new ApiError(400, "No comment ID found!");
+    }
+
+    if(!updatedContent){
+        throw new ApiError(400, "No comment content Found!")
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if (!comment) {
+        throw new ApiError(404, "comment not found!")
+    }
+
+    if (comment.owner._id.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "Unauthorized user to update comments!")
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(commentId, {
+        $set : {
+            content : updatedContent
+        }
+    }, {new : true})
+
+    if (!updatedComment) {
+        throw new ApiError(404, "NO COMMENT FOUND!")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, updatedComment, "Comment updated successfully!")
+    )
+} )
+
+const deleteComment = asyncHandler( async(req, res) => {
+    const { commentId } = req.params;
+
+    if (!commentId) {
+        throw new ApiError(400, "No comment ID found!")
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found!")
+    }
+
+    if (comment.owner._id.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "Unauthorized user to delete comments!")
+    }
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+    if (!deletedComment) {
+        throw new ApiError(404, "No comment was found!")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, deletedComment, "The comment was successfully deleted!")
+    )
+} )
+
 export {
     getVideoComments,
-    addComment
+    addComment,
+    UpdateComment,
+    deleteComment
 }
